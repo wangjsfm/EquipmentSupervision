bb6dda9c7780b5bb79a198aca76450005c478322
w190510995 <wang.1900>
2019-12-27 12:23:55 +0800

    init2

--- src/AirPreheater/Dao/DataHandleDao.py
+++ src/AirPreheater/Dao/DataHandleDao.py
@@ -2,5 +2,12 @@ import  pandas as pd
 
 
 def HandleExcel():
-    pass
+    filepath = '../../Resource/data.xlsx'
+    data = pd.read_excel(filepath)
+    print(data)
+
+
+if __name__ == '__main__':
+    HandleExcel()
+
 
